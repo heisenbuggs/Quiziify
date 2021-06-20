98621d8b45873e1f5f11754bf78ae6ab7e6378a3
coderprasukj <prasukjain0047@gmail.com>
2021-06-21 00:01:41 +0530

    Finally Deployed

---
+++ server/src/db-config.js
@@ -0,0 +1,7 @@
+// TODO: Rename the filename from TODO-db-config.js to db-config.js
+// Paste your MongoDB API key here
+module.exports = {
+	database:
+		'mongodb+srv://prasuk:MMhtnTpYei0oeNwp@cluster0.voak7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+	password: 'MMhtnTpYei0oeNwp',
+}
